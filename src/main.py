205226ac2decc64acc38a0aa70161124a09985da
duoykb <duoykb@gmail.com>
2023-03-29 16:55:15 +0300

    Done!

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from src import moves, tools
 
 
-def find_sequence(start):
+def find_sequence(start):   
+
     def helper(_moves, _visited_moves):
 
         if len(_visited_moves) == 64:
@@ -13,7 +14,7 @@ def find_sequence(start):
                 continue
 
             _visited_moves.append(move)
-            helper(moves.find_available_moves(move), _visited_moves)
+            helper(sorted(list(moves.find_available_moves(move)), key= lambda x: len(list(moves.find_available_moves(x)))), _visited_moves)
             del _visited_moves[-1]
 
     visited_moves = [start]
@@ -26,4 +27,4 @@ def find_sequence(start):
 
 
 tools.display_board()
-print(find_sequence(12))
+print(len(find_sequence(12)))
