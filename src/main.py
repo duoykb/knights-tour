677396526eca38a44935bbc2c0437ddeb10b6da9
duoykb <duoykb@gmail.com>
2023-03-29 16:56:44 +0300

    Done!

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from src import moves, tools
 
 
-def find_sequence(start):   
-
+def find_sequence(start):
     def helper(_moves, _visited_moves):
 
         if len(_visited_moves) == 64:
@@ -14,7 +13,10 @@ def find_sequence(start):
                 continue
 
             _visited_moves.append(move)
-            helper(sorted(list(moves.find_available_moves(move)), key= lambda x: len(list(moves.find_available_moves(x)))), _visited_moves)
+            #  helper(moves.find_available_moves(move), _visited_moves)
+            helper(
+                sorted(list(moves.find_available_moves(move)), key=lambda x: len(list(moves.find_available_moves(x)))),
+                _visited_moves)
             del _visited_moves[-1]
 
     visited_moves = [start]
